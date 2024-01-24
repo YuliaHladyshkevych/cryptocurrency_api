ebc1c5985dbc6f7863aa289ef39dc9c39c01be2c
Yuliia Hladyshkevych <yulia.hladyshkevych@gmail.com>
2024-01-24 14:37:43 +0200

    Test changed settings.py

--- cryptocurrency_api/settings.py
+++ cryptocurrency_api/settings.py
@@ -80,14 +80,24 @@ WSGI_APPLICATION = "cryptocurrency_api.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("POSTGRES_DB"),
+#         "USER": os.getenv("POSTGRES_USER"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+#         "HOST": os.getenv("POSTGRES_HOST"),
+#         "PORT": os.getenv("POSTGRES_PORT"),
+#     }
+# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "NAME": "cryptocurrencies",
+        "USER": "postgres",
+        "PASSWORD": "newpassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
